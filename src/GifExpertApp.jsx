8c407da65d4635e2f8b98da5e3526af636096a00
Alexander <alexander.martinez@lapatota.cl>
2022-08-02 19:06:14 -0400

    Componente AddCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,26 +1,28 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "God of War"]);
 
-    const [categories, setCategories] = useState(['One Punch', 'God of War']);
+  const onAddCategory = () => {
+    setCategories((categories) => [...categories, "Devil May Cry"]);
+  };
 
-    const onAddCategory =  () => {
-        setCategories(categories =>[...categories , 'Devil May Cry']);
-    }
-    
-    return(
-        <>
-            <h1>GifExpertApp</h1>
-            <button onClick={onAddCategory} >Agregar</button>
+  return (
+    <>
+      {/* titulo */}
+      <h1>GifExpertApp</h1>
 
-            <ol>
-                {
-                    categories.map( category =>{
-                        return <li key={category}> {category}</li>
-                    })
-                }
-                
-            </ol>
-        </>
-    )
-}
+      {/* input */}
+      <AddCategory />
+
+      {/* listado de gif */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}> {category}</li>;
+        })}
+      </ol>
+    </>
+  );
+};

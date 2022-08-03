de82d9c69fb1008cdce97a660566de109daacb96
Alexander <alexander.martinez@lapatota.cl>
2022-08-03 13:17:39 -0400

    useEffect

--- src/components/GridGif.jsx
+++ src/components/GridGif.jsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import { getGif } from "../helpers/getGifs";
 
 export const GridGif = ({ category }) => {
-  getGif(category);
+  const [counter, setCounter] = useState(10);
+  // efectos secundarios, si le pasamos [] como segundo argumento
+  // solo se dispararé una única vez
+  useEffect(() => {
+    getGif(category);
+  }, []);
 
   return (
     <>
       <h3>{category}</h3>
+
+      <h5>{counter}</h5>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
     </>
   );
 };
